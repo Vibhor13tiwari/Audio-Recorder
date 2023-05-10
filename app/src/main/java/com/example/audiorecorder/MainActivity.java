bf347cc3ce8044dfe5aa67996adc5ddcceaa7473
Vibhor Tiwari <vibhor13tiwari@gmail.com>
2023-05-10 23:35:58 +0530

    first commit

---
+++ app/src/main/java/com/example/audiorecorder/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.audiorecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContextWrapper;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private static int MICROPHONE_PERMISSION_CODE =200;
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(isMicrophonePresent()){
+            getMicrophonePermission();
+        }
+    }
+
+    public void Record(View view){
+        try{
+            mediaRecorder=new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setOutputFile(getRecordingFilePath());
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.prepare();
+            Toast.makeText(this, "Recording has started", Toast.LENGTH_LONG).show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    private boolean isMicrophonePresent(){
+        if(this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE))
+            return true;
+        else
+            return false;
+    }
+
+    private void getMicrophonePermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)==PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.RECORD_AUDIO},MICROPHONE_PERMISSION_CODE);
+
+    }
+    private String getRecordingFilePath(){
+        ContextWrapper contextWrapper= new ContextWrapper(getApplicationContext());
+        File musicDirectory = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        File file= new File (musicDirectory,"testRecordingFile"+".mp3");
+        return file.getPath();
+    }
+
+}
